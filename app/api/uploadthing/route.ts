bb1db927f0e6d4694a4ef139b3d36f95f71e34e7
Anthony Cueva <acueva@pcienergysolutions.com>
2025-04-13 04:54:53 -0500

    feat: enforce dynamic rendering for upload route
    - Added `dynamic` export with value `force-dynamic` to the upload route for improved handling of dynamic content.

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -3,6 +3,7 @@ import { createRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./core";
 
 export const maxDuration = 90;
+export const dynamic = "force-dynamic";
 
 // Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
