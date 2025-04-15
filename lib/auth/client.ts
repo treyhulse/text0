13460cceed7fd900e61095c84f3e1354ccb53f44
Anthony Cueva <acueva@pcienergysolutions.com>
2025-04-14 19:35:10 -0500

    refactor: remove unused client authentication and update server user retrieval
    - Deleted the unused `useSecureUser` function from the client authentication module.
    - Updated the `getSecureUser` and `getSecureSession` functions in the server module to utilize environment variables for default user IDs, enhancing security and configurability.

--- lib/auth/client.ts
+++
@@ -1,6 +0,0 @@
-import { useUser } from "@clerk/nextjs";
-
-export function useSecureUser() {
-  const { user } = useUser();
-  return user;
-}
--- lib/auth/server.ts
+++ lib/auth/server.ts
@@ -1,12 +1,10 @@
 import { auth, currentUser } from "@clerk/nextjs/server";
 
-const DEFAULT_USER_ID = "user_2vfLrF7SIkim0eRgCHpngMm8gP8";
-
 export async function getSecureUser() {
   const user = await currentUser();
   if (!user) {
     return {
-      id: DEFAULT_USER_ID,
+      id: process.env.DEFAULT_USER_ID,
       fullName: "Cool Visitor",
     };
   }
@@ -17,7 +15,7 @@ export async function getSecureSession() {
   const session = await auth();
   if (!session.userId) {
     return {
-      userId: DEFAULT_USER_ID,
+      userId: process.env.DEFAULT_USER_ID,
     };
   }
   return session;
